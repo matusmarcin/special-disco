978335d138d2cefb270f528d6ecca01a7f6a0bb0
Roman Billík <roman.billik@student.tuke.sk>
2016-10-19 20:38:55 +0200

    commit

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -11,20 +11,12 @@ import messages from './messages';
 
 import styles from './styles.css';
 
-<<<<<<< HEAD
 
 function Footer() {
   return (
     <footer className={styles.footer}>
      <p>© 2016. All rights reserved.</p>
     </footer>
-=======
-function Footer() {
-  return (
-    <div className={styles.footer}>
-      <FormattedMessage {...messages.header} />
-    </div>
->>>>>>> f22eed6dce43265487abcde03aaece7ae922cdec
   );
 }
 
