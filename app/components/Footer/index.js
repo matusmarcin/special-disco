f65047cb9f39d32b39b3fcabb66b26cda5674934
Roman Billík <roman.billik@student.tuke.sk>
2016-10-23 10:15:49 +0200

    social icons

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -17,10 +17,16 @@ function Footer() {
      <footer className={styles.footer}>
     
        <section>
-         <p>
-            <FormattedMessage {...messages.licenseMessage} />
+          <p>
+             <FormattedMessage {...messages.licenseMessage} />
          </p>
+
+
+          <a href="http://twitter.com/minimalmonkey" className="icon-button twitter"><i className="icon-twitter"></i><span></span></a>
+          <a href="http://facebook.com" className="icon-button facebook"><i className="icon-facebook"></i><span></span></a>
+          <a href="http://plus.google.com" className="icon-button google-plus"><i className="icon-google-plus"></i><span></span></a>
        </section>
+
 
     </footer>
   );
