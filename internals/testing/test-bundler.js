d2276507152e9e8bfc66aec0fd63e16ded651882
patrikmajercik2 <majercik.patrik.mmm@gmail.com>
2016-10-27 16:08:16 +0200

    revert back ES6 syntax for test-bundler.js

--- internals/testing/test-bundler.js
+++ internals/testing/test-bundler.js
@@ -1,10 +1,10 @@
 // needed for regenerator-runtime
 // (ES7 generator support is required by redux-saga)
-require('babel-polyfill');
+import 'babel-polyfill';
 
 // If we need to use Chai, we'll have already chaiEnzyme loaded
-const chai = require('chai');
-const chaiEnzyme = require('chai-enzyme');
+import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
 chai.use(chaiEnzyme());
 
 // Include all .js files under `app`, except app.js, reducers.js, and routes.js.
